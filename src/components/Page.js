c9220f904873ba70bc3dc7b62b32c9857685f07a
diana2341 <dianaponce2341@gmail.com>
2020-07-13 13:52:48 -0400

    social

--- src/components/Page.js
+++ src/components/Page.js
@@ -5,29 +5,30 @@ render(){
     return(
         <>
         <div id="mySidenav" className='sidenav'>
-            
+           <p className='job'>Software Engineer</p> 
         <img alt='pic' className="me" src={require('../imgs/me2.JPG')}/> 
 
         {/* <a href="" className="closebtn" onClick={()=>closeNav()}>&times;</a> */}
      <div className="box-nav"> 
+     
              <a href='#home'>Home</a>
                 <img alt='pic' className='icon home'src={require('../imgs/home.png')}/>
                <a href='#about'>About</a>
                <img alt='pic' className='icon woman'src={require('../imgs/woman.png')}/>
-
+             <a href='#projects'>Projects</a>
+                 <img alt='pic' className='icon computer'src={require('../imgs/computer.png')}/> 
                 <a href='#contact'>Contact</a>
                 <img alt='pic' className='icon contact'src={require('../imgs/contact.png')}/>
 
-                <a href='#projects'>Projects</a>
-                 <img alt='pic' className='icon computer'src={require('../imgs/computer.png')}/> 
-
-                <div className='social'>
-                <img alt='pic' className='icon social'src={require('../imgs/git.png')}/>
-                <img alt='pic' className='icon social'src={require('../imgs/instagram.png')}/>
-                <img alt='pic' className='icon social'src={require('../imgs/medium.png')}/>
-                <img alt='pic' className='icon social'src={require('../imgs/linkedin.png')}/>
+      
+            </div> 
+               <div className='social'>
+                <a href="https://github.com/diana2341"><img alt='pic' className='icon social'src={require('../imgs/git.png')}/></a>
+                <a href='https://www.instagram.com/diana.poncee/'><img alt='pic' className='icon social'src={require('../imgs/instagram.png')}/></a>
+                <a href='https://medium.com/@dianaponce2341'><img alt='pic' className='icon social'src={require('../imgs/medium.png')}/></a>
+                <a href='https://www.linkedin.com/in/diana-ponce-1718821a2/'><img alt='pic' className='icon social'src={require('../imgs/linkedin.png')}/></a>
                 </div>
-     </div>
+     
            
           
 
