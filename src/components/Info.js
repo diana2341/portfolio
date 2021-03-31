984f5c86e330c68e1593932a742e82dc51fa23f4
diana2341 <dianaponce2341@gmail.com>
2021-03-31 14:32:41 -0400

    removed unused pics and added app

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,90 +1,55 @@
-import Img1 from '../imgs/animal1.png'
-import Img2 from '../imgs/animal2.png'
-import Img3 from '../imgs/animal3.png'
-import Img4 from '../imgs/animal4.png'
-import sound1 from '../imgs/sound1.png'
-import sound2 from '../imgs/sound2.png'
-import sound3 from '../imgs/sound3.png'
-import home1 from '../imgs/home1.png'
-import home2 from '../imgs/home2.png'
-import home3 from '../imgs/home3.png'
-import home4 from '../imgs/home4.png'
-import home5 from '../imgs/home5.png'
-import home6 from '../imgs/home6.png'
-import cake1 from '../imgs/cake1.png'
-import cake2 from '../imgs/cake2.png'
-import cake3 from '../imgs/cake3.png'
-import cake4 from '../imgs/cake4.png'
-import cake5 from '../imgs/cake5.png'
-import cake6 from '../imgs/cake6.png'
-import cake7 from '../imgs/cake7.png'
-import cake8 from '../imgs/cake8.png'
 import endangered from '../imgs/endangered.png'
 import illucid from '../imgs/illucid.png'
 import cupcake from '../imgs/cupcakes.png'
 import travel from '../imgs/travel.png'
 import shopmart from '../imgs/shopmart.png'
+import taco from '../imgs/taco-haven.png'
 
 
 
 
 const info = [
     {
+        id:6,
+        class:'taco-Haven',
+        main:taco,
+        name: 'Taco-Haven',
+        tools:'Boostrap, CSS,HTML,React',
+        description:'Mexican Restaurant example site ',
+        gitLink:'https://github.com/diana2341/tac-p',
+        deploy:'https://taco-haven-restaurant.netlify.app/'
+
+    },
+    {
         id:5,
         class:'shopmart',
         main:shopmart,
-
         name: 'Shop-mart',
         tools:'Ruby Rails, CSS,HTML,React,Redux',
-        // video:"https://streamable.com/e/ai4u1c",
-        pic1:home1,
-        pic2:home2,
-        pic3:home3,
-        pic4:home4,
-        pic5:home5,
-        pic6:home6,
         description:'A  ecommerce site for clothing powered with  React + Redux ',
         gitLink:'https://github.com/diana2341/shopMart-front',
-        // youtube:'https://streamable.com/ai4u1c',
         deploy:'https://shop-mart-us.netlify.app/'
 
     },
     {
       id:1,
       main:endangered,
-
       class:'endangered',
       name: 'Endangered Lives',
       tools:'React.js, Ruby Rails API, CSS,HTML',
       libraries:'React slide-show-image, React Simple Maps',
       video:"https://www.youtube.com/embed/xTjHOjz2xGA",
-      pic1:Img1,
-      pic2:Img2,
-      pic3:Img3,
-      pic4:Img4,
-
-
-
       description:"An educational site  that raises awareness about several animal species endangered due to poaching. Reasons for poaching are explained to the user. They can also view recent news about the animal and are able to join a community to discuss  the topic with others.",
       gitLink:'https://github.com/diana2341/why-poaching-front',
-      youtube:'https://www.youtube.com/watch?v=xTjHOjz2xGA&t=13s',
-    //   deploy:'https://endangered-lives.herokuapp.com/'
-      
+      youtube:'https://www.youtube.com/watch?v=xTjHOjz2xGA&t=13s',      
     },
     {
         id:4,
         class:'travel',
         main:travel,
-
         name: 'Travel Rails',
         tools:'Ruby Rails, CSS,HTML,Bulma.io',
         video:"https://streamable.com/e/ai4u1c",
-        pic1:home1,
-        pic2:home2,
-        pic3:home3,
-        pic4:home4,
-        pic5:home5,
-        pic6:home6,
         description:'A site for hosting homes as stays for people on vacation and where homes, apartments, or rooms can be booked by people traveling. Users can become hosts and post their homes or find a home.',
         gitLink:'https://github.com/diana2341/My_Travel_rails',
         youtube:'https://streamable.com/ai4u1c',
@@ -96,9 +61,6 @@ const info = [
         name: 'Illucid Beats',
         tools:'Vanilla Javascript, CSS, HTML, Ruby Rails API',
         video:"https://www.youtube.com/embed/pOq3eoo0Mjs",
-        pic1:sound1,
-        pic2:sound2,
-        pic3:sound3,
         description:'A sound machine used to reduce stress, increase concentration or enable the ability to sleep better with ambient noises. Users can make a mix of ambient noises and listen to others creations.',
         gitLink:'https://github.com/diana2341/illucid-beats-front',
         youtube:'https://www.youtube.com/watch?v=pOq3eoo0Mjs',
@@ -108,18 +70,9 @@ const info = [
         id:3,
         class:'sugar',
         main:cupcake,
-
         name: 'Sugar Lips',
         tools:'React.js, Ruby Rails API, CSS,HTML',
         video:"https://www.youtube.com/embed/J1_E9mqnAM8",
-        pic1:cake1,
-        pic2:cake2,
-        pic3:cake3,
-        pic4:cake4,
-        pic5:cake5,
-        pic6:cake6,
-        pic7:cake7,
-        pic8:cake8,
         description:'An ecommerce site for a bakery that sells cupcakes, where  users can order any amount of these treats to be delivered to their homes!',
         gitLink:'https://github.com/diana2341/mod4-sugarlips_cupcakes-client',
         youtube:'https://www.youtube.com/watch?v=J1_E9mqnAM8&t=2s',
